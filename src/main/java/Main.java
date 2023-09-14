614a70c166a17195cf0df117d8a87e987e19c77e
pavelkurtukov <pavel@kurtukov.ru>
2023-09-14 14:01:29 +0700

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,17 @@
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    // Генератор текстов
+    public static String generateText(String letters, int length) {
+        Random random = new Random();
+        StringBuilder text = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            text.append(letters.charAt(random.nextInt(letters.length())));
+        }
+        return text.toString();
+    }
+}
